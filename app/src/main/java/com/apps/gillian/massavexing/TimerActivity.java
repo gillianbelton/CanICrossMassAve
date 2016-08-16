43090e4ec46577cdc0e9313f832678f1990b3d2b
Gillian Patricia Belton <Gillian@Gillians-MacBook-Pro-2.local>
2016-08-16 17:53:19 -0600

    First Commit

---
+++ app/src/main/java/com/apps/gillian/massavexing/TimerActivity.java
@@ -0,0 +1,100 @@
+package com.apps.gillian.massavexing;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.graphics.Typeface;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Timestamp;
+
+public class TimerActivity extends AppCompatActivity {
+
+    private static final int STOPLIGHT_DURATION = 30;
+    private static final Long BASE_TIME = 1470729402L;
+
+    private TextView tvTimer;
+    private CountDownTimer countDownTimer;
+    private Long currentTimeMillis;
+    private int finalTime;
+    private boolean firstTime;
+    private boolean canWalk;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+
+        //set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView tbTitle = (TextView) toolbar.findViewById(R.id.tb_title);
+        tbTitle.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Knile-Regular.ttf"));
+
+        //set up timer
+        tvTimer = (TextView) findViewById(R.id.tvTimer);
+        firstTime = true;
+        setCurrentTime();
+        resetTimer();
+    }
+
+    private void setCurrentTime() {
+        currentTimeMillis = System.currentTimeMillis();
+        Long currentTimeSecs = currentTimeMillis/1000;
+        finalTime = (int) ((currentTimeSecs - BASE_TIME) % STOPLIGHT_DURATION);
+        canWalk = (((currentTimeSecs - BASE_TIME)/STOPLIGHT_DURATION) % 2) == 0;
+        setWalkingSign();
+    }
+
+    private void setWalkingSign() {
+        ImageView canWalkSymbol = (ImageView) findViewById(R.id.iv_walk_symbol);
+        if (!canWalk) {
+            canWalkSymbol.setImageDrawable(getResources().getDrawable(R.drawable.cross_walk_false));
+            canWalk = true;
+        }
+        else {
+            canWalkSymbol.setImageDrawable(getResources().getDrawable(R.drawable.cross_walk_true));
+            canWalk = false;
+        }
+    }
+
+    public void resetTimer(){
+        if (firstTime) {
+            countDownTimer = new CountDownTimer(finalTime * 1000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    tvTimer.setText(" " + millisUntilFinished / 1000 + " ");
+                }
+
+                public void onFinish() {
+                    resetTimer();
+                }
+            };
+            countDownTimer.start();
+            firstTime = false;
+        }
+        else {
+            countDownTimer = new CountDownTimer((STOPLIGHT_DURATION - 1) * 1000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    tvTimer.setText(" " + millisUntilFinished / 1000 + " ");
+                }
+
+                public void onFinish() {
+                    resetTimer();
+                }
+            };
+            setWalkingSign();
+            countDownTimer.start();
+        }
+    }
+}
